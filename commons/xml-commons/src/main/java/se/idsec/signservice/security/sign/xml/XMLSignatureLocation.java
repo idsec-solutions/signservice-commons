8bcd3dd2483472169c1a68d2918036bd894f2ec4
Martin Lindström <martin.lindstrom@litsec.se>
2019-12-09 16:45:27 +0100

    XML signature position selection

---
+++ commons/xml-commons/src/main/java/se/idsec/signservice/security/sign/xml/XMLSignatureLocation.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2019 IDsec Solutions AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.idsec.signservice.security.sign.xml;
+
+import javax.annotation.Nonnull;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.apache.xml.security.utils.XMLUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Tells where in an XML document the signature should be inserted.
+ * 
+ * @author Martin Lindström (martin@idsec.se)
+ * @author Stefan Santesson (stefan@idsec.se)
+ */
+@Slf4j
+public class XMLSignatureLocation {
+
+  /**
+   * Enum for indicating the insertion point within a selected parent node.
+   */
+  public enum ChildPosition {
+    FIRST, LAST
+  };
+
+  /** Indicator for first or last child of a selected parent node. */
+  private ChildPosition childPosition;
+
+  /**
+   * The XPath expression for selecting the parent node (or {@code null} which means the the parent node is the document
+   * root element).
+   */
+  private XPathExpression xPathExpression;
+
+  /** The textual representation of the XPath expression (for logging). */
+  private String xPath;
+
+  /**
+   * Constructor setting up the signature location to "the last child of the document root element".
+   */
+  public XMLSignatureLocation() {
+    this(ChildPosition.LAST);
+  }
+
+  /**
+   * Constructor setting of the signature location to "the first child of the document root element"
+   * ({@code childPosition} == {@link ChildPosition#FIRST} or "the last child of the document root element"
+   * ({@code childPosition} == {@link ChildPosition#LAST}.
+   * 
+   * @param childPosition
+   *          first of last child of the document root element
+   */
+  public XMLSignatureLocation(@Nonnull final ChildPosition childPosition) {
+    this.childPosition = childPosition;
+  }
+
+  /**
+   * Constructor accepting an XPath expression for finding the parent element of where we should insert the signature
+   * element. Note that the result of evaluating the XPath expression <b>MUST</b> be one single node.
+   * 
+   * @param parentXPath
+   *          XPath expression for locating the parent node of the signature element
+   * @param childPosition
+   *          whether to insert the signature as the first or last child of the given parent node
+   * @throws XPathExpressionException
+   *           for illegal XPath expressions
+   */
+  public XMLSignatureLocation(@Nonnull final String parentXPath, @Nonnull final ChildPosition childPosition)
+      throws XPathExpressionException {
+    this.childPosition = childPosition;
+    this.xPath = parentXPath;
+    this.xPathExpression = XPathFactory.newInstance().newXPath().compile(parentXPath);
+  }
+
+  /**
+   * Inserts the given {@code Signature} element into the document according to this object's configuration.
+   * <p>
+   * Note: If the owner document of the given {@code Signature} element is not the same as the {@code document}
+   * paramater, the element is imported into this document.
+   * </p>
+   * 
+   * @param signature
+   *          the element to insert
+   * @param document
+   *          the document to which the signature element should be inserted
+   * @throws XPathExpressionException
+   *           for XPath selection errors
+   */
+  public void insertSignature(@Nonnull final Element signature, @Nonnull final Document document) throws XPathExpressionException {
+
+    // If the signature element comes from a different document, import it.
+    final boolean sameOwner = XMLUtils.getOwnerDocument(signature) == document;
+    Node signatureNode = sameOwner ? signature : document.importNode(signature, true);
+
+    Node parentNode = this.xPathExpression != null
+        ? (Node) this.xPathExpression.evaluate(document, XPathConstants.NODE)
+        : document.getDocumentElement();
+
+    if (parentNode == null) {
+      // Node was not found
+      final String msg = String.format("Could not find XML node for insertion of Signature - XPath: %s", xPath);
+      log.error(msg);
+      throw new XPathExpressionException(msg);
+    }
+
+    // If the XPath "/" was given we help a bit ...
+    if (parentNode.getNodeType() == Node.DOCUMENT_NODE) {
+      parentNode = ((Document) parentNode).getDocumentElement();
+    }
+
+    if (ChildPosition.LAST == this.childPosition) {
+      parentNode.appendChild(signatureNode);
+    }
+    else {
+      parentNode.insertBefore(signatureNode, parentNode.getFirstChild());
+    }
+  }
+
+}
---
+++ commons/xml-commons/src/test/java/se/idsec/signservice/security/sign/xml/XMLSignatureLocationTest.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2019 IDsec Solutions AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.idsec.signservice.security.sign.xml;
+
+import java.io.InputStream;
+
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import se.idsec.signservice.security.sign.xml.XMLSignatureLocation.ChildPosition;
+import se.swedenconnect.opensaml.OpenSAMLInitializer;
+import se.swedenconnect.opensaml.OpenSAMLSecurityDefaultsConfig;
+import se.swedenconnect.opensaml.OpenSAMLSecurityExtensionConfig;
+import se.swedenconnect.opensaml.xmlsec.config.SAML2IntSecurityConfiguration;
+
+/**
+ * Tests for {@code XMLSignatureLocation}.
+ * 
+ * @author Martin Lindström (martin@idsec.se)
+ * @author Stefan Santesson (stefan@idsec.se)
+ */
+public class XMLSignatureLocationTest {
+
+  @BeforeClass
+  public static void initializeOpenSAML() throws Exception {
+    OpenSAMLInitializer.getInstance().initialize(
+      new OpenSAMLSecurityDefaultsConfig(new SAML2IntSecurityConfiguration()),
+      new OpenSAMLSecurityExtensionConfig());
+  }
+
+  @Test
+  public void testDefault() throws Exception {
+
+    XMLSignatureLocation location = new XMLSignatureLocation();
+
+    Document empty = getDocument("xml/empty.xml");
+    location.insertSignature(createSignatureElement(), empty);
+    Assert.assertEquals("Signature", getLastElement(empty));
+
+    Document simple = getDocument("xml/simple.xml");
+    location.insertSignature(createSignatureElement(), simple);
+    Assert.assertEquals("Signature", getLastElement(simple));
+
+    // Make sure it works even if signature has the same document owner
+    Document simple2 = getDocument("xml/simple.xml");
+    location.insertSignature((Element) simple2.importNode(createSignatureElement(), true), simple2);
+    Assert.assertEquals("Signature", getLastElement(simple2));
+
+    Document signRequest = getDocument("xml/signRequest.xml");
+    location.insertSignature(createSignatureElement(), signRequest);
+    Assert.assertEquals("Signature", getLastElement(signRequest));
+  }
+
+  @Test
+  public void testFirst() throws Exception {
+    XMLSignatureLocation location = new XMLSignatureLocation(ChildPosition.FIRST);
+
+    Document empty = getDocument("xml/empty.xml");
+    location.insertSignature(createSignatureElement(), empty);
+    Assert.assertEquals("Signature", getFirstElement(empty));
+
+    Document simple = getDocument("xml/simple.xml");
+    location.insertSignature(createSignatureElement(), simple);
+    Assert.assertEquals("Signature", getFirstElement(simple));
+
+    Document signRequest = getDocument("xml/signRequest.xml");
+    location.insertSignature(createSignatureElement(), signRequest);
+    Assert.assertEquals("Signature", getFirstElement(signRequest));
+  }
+
+  @Test
+  public void testXPathBasic() throws Exception {
+
+    String[] xpaths = new String[] { "/", "/Root" };
+    for (String xpath : xpaths) {
+      XMLSignatureLocation location = new XMLSignatureLocation(xpath, ChildPosition.LAST);
+
+      Document doc = getDocument("xml/empty.xml");
+      location.insertSignature(createSignatureElement(), doc);
+      Assert.assertEquals(
+        String.format("Document: empty.xml, xPath: %s", xpath),
+        "Signature", getLastElement(doc));
+    }
+    for (String xpath : xpaths) {
+      XMLSignatureLocation location = new XMLSignatureLocation(xpath, ChildPosition.FIRST);
+
+      Document doc = getDocument("xml/empty.xml");
+      location.insertSignature(createSignatureElement(), doc);
+      Assert.assertEquals(
+        String.format("Document: empty.xml, xPath: %s", xpath),
+        "Signature", getFirstElement(doc));
+    }
+
+    XMLSignatureLocation location = new XMLSignatureLocation("/NotElement", ChildPosition.LAST);
+    Document doc = getDocument("xml/empty.xml");
+    try {
+      location.insertSignature(createSignatureElement(), doc);
+      Assert.fail("Expected XPathExpressionException");
+    }
+    catch (XPathExpressionException e) {
+    }
+
+    xpaths = new String[] { "/TheRoot/ElementOne", "/*/ElementOne" };
+    for (String xpath : xpaths) {
+      location = new XMLSignatureLocation(xpath, ChildPosition.LAST);
+
+      doc = getDocument("xml/simple.xml");
+      location.insertSignature(createSignatureElement(), doc);
+      Assert.assertEquals(
+        String.format("Document: simple.xml, xPath: %s", xpath),
+        "Signature", getLastElement(doc, "ElementOne"));
+    }
+    for (String xpath : xpaths) {
+      location = new XMLSignatureLocation(xpath, ChildPosition.FIRST);
+
+      doc = getDocument("xml/simple.xml");
+      location.insertSignature(createSignatureElement(), doc);
+      Assert.assertEquals(
+        String.format("Document: simple.xml, xPath: %s", xpath),
+        "Signature", getFirstElement(doc, "ElementOne"));
+    }
+  }
+
+  @Test
+  public void testXPathMultiple() throws Exception {
+    String[] xpaths = new String[] { "/TheRoot/Element", "/*/Element", "//Element[1]" };
+    for (String xpath : xpaths) {
+      XMLSignatureLocation location = new XMLSignatureLocation(xpath, ChildPosition.LAST);
+
+      Document doc = getDocument("xml/multipleElements.xml");
+      location.insertSignature(createSignatureElement(), doc);
+
+      Assert.assertEquals(
+        String.format("Document: multipleElements.xml, xPath: %s", xpath),
+        "Signature", getLastElement(doc, "Element"));
+    }
+    // Select the second Element.
+    //
+    String xpath = "/TheRoot/Element[2]";
+    XMLSignatureLocation location = new XMLSignatureLocation(xpath, ChildPosition.LAST);
+
+    Document doc = getDocument("xml/multipleElements.xml");
+    location.insertSignature(createSignatureElement(), doc);
+    
+    Assert.assertEquals(
+      String.format("Document: multipleElements.xml, xPath: %s", xpath),
+      "Signature",
+      doc.getDocumentElement().getElementsByTagName("Element").item(1).getChildNodes()
+        .item(doc.getDocumentElement().getElementsByTagName("Element").item(1).getChildNodes().getLength() - 1).getLocalName());
+
+    // Select fails
+    //
+    xpath = "/TheRoot/Element[3]";
+    location = new XMLSignatureLocation(xpath, ChildPosition.LAST);
+
+    doc = getDocument("xml/multipleElements.xml");
+    try {
+      location.insertSignature(createSignatureElement(), doc);
+      Assert.fail("Expected XPathExpressionException");
+    }
+    catch (XPathExpressionException e) {
+    }
+  }
+
+  @Test
+  public void testXPathSignRequest() throws Exception {
+    XMLSignatureLocation location = new XMLSignatureLocation("/*/OptionalInputs", ChildPosition.LAST); 
+        
+    Document signRequest = getDocument("xml/signRequest.xml");
+    location.insertSignature(createSignatureElement(), signRequest);
+    
+    // System.out.println(SerializeSupport.prettyPrintXML(signRequest));
+    
+    Node node = signRequest.getDocumentElement().getElementsByTagName("dss:OptionalInputs").item(0);    
+    Assert.assertEquals("Signature", node.getChildNodes().item(node.getChildNodes().getLength() - 1).getLocalName()); 
+  }
+  
+  private static String getFirstElement(Document doc) {
+    return doc.getDocumentElement().getChildNodes().item(0).getLocalName();
+  }
+
+  private static String getFirstElement(Document doc, String parent) {
+    return doc.getDocumentElement().getElementsByTagName(parent).item(0).getChildNodes().item(0).getLocalName();
+  }
+
+  private static String getLastElement(Document doc) {
+    return doc.getDocumentElement().getChildNodes().item(doc.getDocumentElement().getChildNodes().getLength() - 1).getLocalName();
+  }
+
+  private static String getLastElement(Document doc, String parent) {
+    Node node = doc.getDocumentElement().getElementsByTagName(parent).item(0);
+    return node.getChildNodes().item(node.getChildNodes().getLength() - 1).getLocalName();
+  }
+
+  private static Document getDocument(String path) throws Exception {
+    Resource resource = new ClassPathResource(path);
+    InputStream is = resource.getInputStream();
+    try {
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+      dbf.setIgnoringComments(false);
+      DocumentBuilder builder = dbf.newDocumentBuilder();
+      return builder.parse(is);
+    }
+    finally {
+      is.close();
+    }
+  }
+
+  private static Element createSignatureElement() throws Exception {
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = dbf.newDocumentBuilder();
+    Document doc = builder.newDocument();
+
+    Element element = doc.createElementNS(XMLSignature.XMLNS, "ds:Signature");
+    doc.appendChild(element);
+
+    element.setTextContent("Test");
+
+    return element;
+  }
+
+}
