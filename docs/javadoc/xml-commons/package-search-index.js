e25fea66d51d85edd7ec1b2103811dee5f2b477a
Martin Lindström <martin.lindstrom@litsec.se>
2023-10-21 09:31:14 +0200

    2.0.0 javadoc

--- docs/javadoc/xml-commons/package-search-index.js
+++ docs/javadoc/xml-commons/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"se.idsec.signservice.security.sign.xml"},{"l":"se.idsec.signservice.security.sign.xml.impl"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"se.idsec.signservice.security.sign.xml"},{"l":"se.idsec.signservice.security.sign.xml.impl"}];updateSearchResults();
